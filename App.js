2881ba9dab8832d4d280ff76c99c2b0acfaeb844
Julian Akselrad <111514246+JuliiAk47@users.noreply.github.com>
2023-07-05 09:10:09 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View,Image, ImageBackground } from 'react-native';
 import React from 'react';
 import Card from './Card';
+import friends from "./friends.jpg"
 
 export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Image source={require('./logo.png')} style={{ width: 300, height: 150 }} />
-      <Text style={{ fontSize: 20, marginVertical: 25, fontWeight: 'bold', fontFamily: 'Roboto'}}>Try some of our most popular flavors!</Text>
+      <Text style={{ fontSize: 20, marginVertical: 25, fontWeight: 'bold', fontFamily: 'Arial'}}>Try some of our most popular flavors!</Text>
       <Card />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
--- Card.js
+++ Card.js
@@ -3,9 +3,9 @@ import { View, Image, Text } from 'react-native';
 
 const Card = () => {
   return (
-    <View style={{ backgroundColor: '#fff', padding: 20, borderRadius: 10, shadowColor: '#000', shadowOpacity: 0.2, shadowOffset: { width: 0, height: 2 }, shadowRadius: 4, elevation: 2 }}>
-      <Image source={require('./cafe.jpg')} style={{ width: 200, height: 200, marginBottom: 10 }} />
-      <Text style={{ fontSize: 18,textAlign:'center', fontWeight:'bold' }}>Mocha</Text>
+    <View style={{ backgroundColor: '#fff', padding: 20, borderRadius: 30, shadowColor: '#000', shadowOpacity: 0.2, shadowOffset: { width: 0, height: 2 }, shadowRadius: 4, elevation: 2 }}>
+      <Image source={require('./cafe.jpg')} style={{ width: 300, height: 300, marginBottom: 10 }} />
+      <Text style={{ fontSize: 20,textAlign:'center', fontWeight:'bold' }}>Mocha</Text>
     </View>
   );
 };
